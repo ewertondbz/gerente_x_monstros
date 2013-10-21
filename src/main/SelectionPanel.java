ca17d7da060bdcfea5f08914046fb5d289bc55ec
ewertondbz <ewertondbz@hotmail.com>
2013-10-21 00:13:48 -0200

    Sincronização apenas

--- src/main/SelectionPanel.java
+++ src/main/SelectionPanel.java
@@ -4,6 +4,8 @@ import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Point;
 import java.awt.Rectangle;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 
 import javax.swing.JPanel;
 
@@ -40,5 +42,39 @@ public class SelectionPanel extends JPanel {
 
 	private Point adjust(Point point) {
 		return new Point((point.x / WIDTH) * WIDTH, (point.y / HEIGHT) * HEIGHT);
+	}
+
+	public void allowAddTurret() {
+		addMouseListener((new MouseListener() {
+
+			@Override
+			public void mouseReleased(MouseEvent arg0) {
+				// TODO Auto-generated method stub
+
+			}
+
+			@Override
+			public void mousePressed(MouseEvent arg0) {
+				// TODO Auto-generated method stub
+
+			}
+
+			@Override
+			public void mouseExited(MouseEvent arg0) {
+				// TODO Auto-generated method stub
+
+			}
+
+			@Override
+			public void mouseEntered(MouseEvent arg0) {
+				// TODO Auto-generated method stub
+
+			}
+
+			@Override
+			public void mouseClicked(MouseEvent arg0) {
+				// TODO continua daqui
+			}
+		}));
 	}
 }
